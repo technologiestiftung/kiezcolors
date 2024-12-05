954a3df1a99761b95b9727b10ba0dc2fa2664ebb
hhack <hans.f.hack@googlemail.com>
2024-12-05 11:44:09 +0100

    add edit icon next to text

--- src/lib/components/postcard/PostcardFront.svelte
+++ src/lib/components/postcard/PostcardFront.svelte
@@ -323,6 +323,20 @@ https://observablehq.com/@d3/treemap
       : ""}
     class:w-full={$screenWidth <= 444 ? `` : "w-full"}
   />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="18"
+    height="18"
+    fill="#f64c72"
+    class="bi bi-pen-fill text-primary pointer-events-none"
+    viewBox="0 0 16 16"
+    style={$isMobile
+      ? `display:none`
+      : "position: absolute; z-index: 1000; right: 12px; bottom: 65px;"}
+    ><path
+      d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001"
+    /></svg
+  >
 </div>
 
 <style>
